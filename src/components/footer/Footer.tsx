6c630da75001ab1505ec4063cf375443d6f9d994
Massolinii <massimilianoesposito548@gmail.com>
2023-09-08 20:38:59 +0200

    Navbar e Footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <div className="w-full h-20 bg-red-400">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            Made by Massimiliano Esposito. 2023. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
